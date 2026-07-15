058dbe976608878225ed842c58413cf925e9ebc7
jdjuan <david.juanherrera@gmail.com>
2026-07-14 21:20:46 -0500

    redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://jdjuan.io/nine/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
